0f7cd2e30e09bbac0dcdcd1584be234883125149
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-10-29 21:39:03 +0600

    Add files via upload

--- test.php
+++ test.php
@@ -1,6 +1,33 @@
 <?php 
 
-echo bin2hex(random_bytes(256));
+$config = array(
+    "digest_alg" => "sha512",
+    "private_key_bits" => 4096,
+    "private_key_type" => OPENSSL_KEYTYPE_RSA,
+);
+   
+// Create the private and public key
+$res = openssl_pkey_new($config);
+
+// Extract the private key from $res to $privKey
+openssl_pkey_export($res, $privKey);
+
+// Extract the public key from $res to $pubKey
+$pubKey = openssl_pkey_get_details($res);
+$pubKey = $pubKey["key"];
+$r = "[crypt]\npublic=$pubKey\nprivate=$privKey";
+$fp = fopen("replica.my.cnf","w");
+file_put_contents($fp,$r);
+fclose($fp);
+$data = 'plaintext data goes here';
+
+// Encrypt the data to $encrypted using the public key
+openssl_public_encrypt($data, $encrypted, $pubKey);
+
+// Decrypt the data using the private key and store the results in $decrypted
+openssl_private_decrypt($encrypted, $decrypted, $privKey);
+
+echo $decrypted;
 // Store a string into the variable which 
 // need to be Encrypted 
 
