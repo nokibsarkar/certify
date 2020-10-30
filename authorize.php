97c0de7bbc2d460d5d26038bdf74ceb609314edb
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-10-30 10:28:33 +0600

    Add files via upload

---
+++ authorize.php
@@ -0,0 +1,190 @@
+<?php
+session_start();
+/*if(!isset($_SESSION['consumer']) || !isset($_SESSION['user']))
+	header("Location:login.php");
+if(!isset($_GET['oauth_verifier']) ||! isset($_GET['oauth_token']))
+	header("Location: index.php");*/
+$gConsumerKey = $_SESSION['consumer']['Key'];
+$gConsumerSecret = $_SESSION['consumer']['Secret'];
+$gUserAgent = $_SESSION['consumer']['Agent'];
+$gTokenKey = $_SESSION['tokenKey'];
+$gTokenSecret=$_SESSION['tokenSecret'];
+/**
+ * Set this to the Special:OAuth URL. 
+ * Note that /wiki/Special:OAuth fails when checking the signature, while
+ * index.php?title=Special:OAuth works fine.
+ */
+$mwOAuthUrl = 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth';
+
+/**
+ * Set this to the interwiki prefix for the OAuth central wiki.
+ */
+$mwOAuthIW = 'meta';
+/**
+ * Handle a callback to fetch the access token
+ * @return void
+ */
+ /**
+ * Utility function to sign a request
+ *
+ * Note this doesn't properly handle the case where a parameter is set both in 
+ * the query string in $url and in $params, or non-scalar values in $params.
+ *
+ * @param string $method Generally "GET" or "POST"
+ * @param string $url URL string
+ * @param array $params Extra parameters for the Authorization header or post 
+ * 	data (if application/x-www-form-urlencoded).
+ * @return string Signature
+ */
+ function sign_request( $method, $url, $params = array() ) {
+ global $gConsumerSecret, $gTokenSecret;
+ $parts = parse_url( $url );
+ // We need to normalize the endpoint URL
+ $scheme = isset( $parts['scheme'] ) ? $parts['scheme'] : 'http';
+ $host = isset( $parts['host'] ) ? $parts['host'] : '';
+ $port = isset( $parts['port'] ) ? $parts['port'] : ( $scheme == 'https' ? '443' : '80' );
+ $path = isset( $parts['path'] ) ? $parts['path'] : '';
+ if ( ( $scheme == 'https' && $port != '443' ) ||
+ ( $scheme == 'http' && $port != '80' ) 
+ ) {
+ // Only include the port if it's not the default
+ $host = "$host:$port";
+ }
+ 
+ // Also the parameters
+ $pairs = array();
+ parse_str( isset( $parts['query'] ) ? $parts['query'] : '', $query );
+ $query += $params;
+ unset( $query['oauth_signature'] );
+ if ( $query ) {
+ $query = array_combine(
+ // rawurlencode follows RFC 3986 since PHP 5.3
+ array_map( 'rawurlencode', array_keys( $query ) ),
+ array_map( 'rawurlencode', array_values( $query ) )
+ );
+ ksort( $query, SORT_STRING );
+ foreach ( $query as $k => $v ) {
+ $pairs[] = "$k=$v";
+ }
+ }
+ 
+ $toSign = rawurlencode( strtoupper( $method ) ) . '&' .
+ rawurlencode( "$scheme://$host$path" ) . '&' .
+ rawurlencode( join( '&', $pairs ) );
+ $key = rawurlencode( $gConsumerSecret ) . '&' . rawurlencode( $gTokenSecret );
+ return base64_encode( hash_hmac( 'sha1', $toSign, $key, true ) );
+ }
+
+function fetchIdentity($action='token') {
+	global $mwOAuthUrl, $gUserAgent, $gConsumerKey, $gTokenKey, $gTokenSecret, $errorCode;
+	$url = $mwOAuthUrl . $action;
+	$url .= strpos( $url, '?' ) ? '&' : '?';
+	$url .= http_build_query( array(
+		'format' => 'json',
+		'oauth_verifier' => $_GET['oauth_verifier'],
+
+		// OAuth information
+		'oauth_consumer_key' => $gConsumerKey,
+		'oauth_token' => $gTokenKey,
+		'oauth_version' => '1.0',
+		'oauth_nonce' => md5( microtime() . mt_rand() ),
+		'oauth_timestamp' => time(),
+
+		// We're using secret key signatures here.
+		'oauth_signature_method' => 'HMAC-SHA1'
+	) );
+	$signature = sign_request( 'GET', $url );
+	$url .= "&oauth_signature=" . urlencode( $signature );
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+	curl_setopt( $ch, CURLOPT_USERAGENT, $gUserAgent );
+	curl_setopt( $ch, CURLOPT_HEADER, 0 );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	$data = curl_exec( $ch );
+	if ( !$data ) {
+	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Curl error: ' . htmlspecialchars( curl_error( $ch ) );
+	//	exit(0);
+	}
+	curl_close( $ch );
+	$token = json_decode( $data, true);
+	var_dump($token);
+	// Save the access token
+	//$_SESSION['tokenKey'] = $gTokenKey = $token['key'];
+	//$_SESSION['tokenSecret'] = $gTokenSecret = $token['secret'];
+}
+fetch('identify');
+/*//fetch User info
+$data = [
+	'action'=>'query',
+	'meta'=>'userinfo',
+	'format'=>'json',
+	'utf8'=>true
+];
+$ch = curl_init();
+$apiUrl = 'https://bn.wikipedia.org/w/api.php';
+/**
+ * Send an API query with OAuth authorization
+ *
+ * @param array $post Post data
+ * @param object $ch Curl handle
+ * @return array API results
+ */
+/*function doApiQuery( $post, &$ch = null ) {
+	global $apiUrl, $gUserAgent, $gConsumerKey, $gTokenKey, $errorCode;
+
+	$headerArr = array(
+		// OAuth information
+		'oauth_consumer_key' => $gConsumerKey,
+		'oauth_token' => $gTokenKey,
+		'oauth_version' => '1.0',
+		'oauth_nonce' => md5( microtime() . mt_rand() ),
+		'oauth_timestamp' => time(),
+
+		// We're using secret key signatures here.
+		'oauth_signature_method' => 'HMAC-SHA1',
+	);
+	$signature = sign_request( 'POST', $apiUrl, $post + $headerArr );
+	$headerArr['oauth_signature'] = $signature;
+
+	$header = array();
+	foreach ( $headerArr as $k => $v ) {
+		$header[] = rawurlencode( $k ) . '="' . rawurlencode( $v ) . '"';
+	}
+	$header = 'Authorization: OAuth ' . join( ', ', $header );
+
+	if ( !$ch ) {
+		$ch = curl_init();
+	}
+	curl_setopt( $ch, CURLOPT_POST, true );
+	curl_setopt( $ch, CURLOPT_URL, $apiUrl );
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $post ) );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, array( $header ) );
+	//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+	curl_setopt( $ch, CURLOPT_USERAGENT, $gUserAgent );
+	curl_setopt( $ch, CURLOPT_HEADER, 0 );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	$data = curl_exec( $ch );
+	if ( !$data ) {
+		//header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Curl error: ' . htmlspecialchars( curl_error( $ch ) );
+		//exit(0);
+	}
+	$ret = json_decode( $data, true);
+	if ( $ret == []) {
+	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
+		echo 'Unparsable API response: <pre>' . htmlspecialchars( $data ) . '</pre>';
+		//exit(0);
+	}
+	return $ret;
+}
+$res = doApiQuery($data, $ch);
+curl_close($ch);
+$_SESSION['user']=[
+	'name'=>$res['query']['userinfo']
+];*/
+session_write_close();
+//$return = isset($_SESSION['return'])?urldecode($_SESSION['return']):'index.php';
+//header("Location: $return");
+?>
--- test.php
+++ test.php
@@ -1,68 +1,145 @@
 <?php 
-$encryption_key = bin2hex(random_bytes(4096));
-echo $encryption_key."<br/>";
-// Store a string into the variable which 
-// need to be Encrypted 
+$inifile = 'oauth.ini';
+$ini = parse_ini_file( $inifile );
+if ( $ini === false ) {
+	echo 'The ini file could not be read';
+	 exit(0);
+}
+if(
+	!isset($ini['gAgent']) ||
+	!isset( $ini['gConsumerKey'] ) ||
+	!isset( $ini['gConsumerSecret'] )
+	){
+	   //header( "HTTP/1.1 $errorCode Internal Server Error" );
+	   echo 'Required configuration directives not found in ini file';
+	   exit(0);
+	   }
+	   var_dump($ini);
+	   $gUserAgent = $ini['gAgent'];
+	   	$gConsumerKey = $ini['gConsumerKey'];
+	   	$gConsumerSecret = $ini['gConsumerSecret'];
+	   	$_SESSION['consumer'] = [
+	   		'Key'=> $gConsumerKey,
+	   		'Secret'=> $gConsumerSecret,
+	   		'Agent'=> $gUserAgent
+	   	];
+// Load the user token (request or access) from the session
+$gTokenKey = '';
+$gTokenSecret = '';
+/**
+* Set this to the Special:OAuth/authorize URL. 
+* To work around MobileFrontend redirection, use /wiki/ rather than /w/index.php.
+*/
+$mwOAuthAuthorizeUrl = 'https://meta.wikimedia.org/wiki/Special:OAuth/authorize';
 
-$simple_string = "Welcome to GeeksforGeeks\n"; 
+/**
+* Set this to the Special:OAuth URL. 
+* Note that /wiki/Special:OAuth fails when checking the signature, while
+* index.php?title=Special:OAuth works fine.
+*/
+$mwOAuthUrl = 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth';
 
-  
-// Display the original string 
+/**
+* Set this to the interwiki prefix for the OAuth central wiki.
+*/
+$mwOAuthIW = 'meta';
+$errorCode = 200;
+function sign_request( $method, $url, $params = array() ) {
+	global $gConsumerSecret, $gTokenSecret;
+	$parts = parse_url($url);
+	// We need to normalize the endpoint URL
+	$scheme = isset($parts['scheme']) ? $parts['scheme'] : 'http';
+	$host = isset($parts['host']) ? $parts['host'] : '';
+	$port = isset($parts['port']) ? $parts['port'] : ($scheme == 'https' ? '443' : '80');
+	$path = isset($parts['path']) ? $parts['path'] : '';
+	if (($scheme == 'https' && $port != '443' ) ||
+		($scheme == 'http' && $port != '80' ) 
+	){
+		// Only include the port if it's not the default
+		$host = "$host:$port";
+	}
+	// Also the parameters
+	$pairs = array();
+	parse_str( isset( $parts['query'] ) ? $parts['query'] : '', $query );
+	$query += $params;
+	unset($query['oauth_signature']);
+	if($query){
+		$query = array_combine(
+		// rawurlencode follows RFC 3986 since PHP 5.3
+			array_map( 'rawurlencode', array_keys( $query ) ),
+			array_map( 'rawurlencode', array_values( $query ) )
+		);
+	ksort($query,SORT_STRING );
+	foreach ($query as $k => $v)
+		$pairs[] = "$k=$v";
+	}
+	$toSign = rawurlencode( strtoupper( $method ) ) . '&' .
+	rawurlencode( "$scheme://$host$path" ) . '&' .
+	rawurlencode( join( '&', $pairs ) );
+	$key = rawurlencode( $gConsumerSecret ) . '&' . rawurlencode( $gTokenSecret );
+	return base64_encode( hash_hmac( 'sha1', $toSign, $key, true ) );
+}
+/**End signature function**/
+/**
+* Request authorization
+* @return void*/
+// First, we need to fetch a request token.
+// The request is signed with an empty token secret and no token key.
+$gTokenSecret = '';
+$url = $mwOAuthUrl . '/initiate';
+$url .= strpos( $url, '?' ) ? '&' : '?';
+$url .= http_build_query( array(
+'format' => 'json',
 
-echo "Original String: " . $simple_string; 
+// OAuth information
+'oauth_callback' => 'https://goodarticlebot.toolforge.org/authorize.php', // Must be "oob" or something prefixed by the configured callback URL
+'oauth_consumer_key' => $gConsumerKey,
+'oauth_version' => '1.0',
+'oauth_nonce' => md5( microtime() . mt_rand() ),
+'oauth_timestamp' => time(),
 
-  
-// Store the cipher method 
+// We're using secret key signatures here.
+'oauth_signature_method' => 'HMAC-SHA1',
+) );
+$signature = sign_request( 'GET', $url );
+$url .= "&oauth_signature=" . urlencode( $signature );
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $url );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch, CURLOPT_USERAGENT, $gUserAgent );
+curl_setopt( $ch, CURLOPT_HEADER, 0 );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+$data = curl_exec( $ch );
+if ( !$data ) {
+   	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
+echo 'Curl error: ' . htmlspecialchars( curl_error( $ch ) );
+exit(0);
+}
+curl_close( $ch );
+$token = json_decode( $data );
+if ( is_object( $token ) && isset( $token->error ) ) {
+   	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
+echo 'Error retrieving token: ' . htmlspecialchars( $token->error ) . '<br>' . htmlspecialchars( $token->message );
+exit(0);
+}
+if ( !is_object( $token ) || !isset( $token->key ) || !isset( $token->secret ) ) {
+   	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
+echo 'Invalid response from token request';
+exit(0);
+}
 
-$ciphering = "AES-128-CTR"; 
+// Now we have the request token, we need to save it for later.
+$_SESSION['tokenKey'] = $token->key;
+$_SESSION['tokenSecret'] = $token->secret;
+session_write_close();
 
-  
-// Use OpenSSl Encryption method 
-
-$iv_length = openssl_cipher_iv_length($ciphering); 
-
-$options = 0; 
-
-  
-// Non-NULL Initialization Vector for encryption 
-
-$encryption_iv = '1234567891011121'; 
-
-  
-
-  
-// Use openssl_encrypt() function to encrypt the data 
-
-$encryption = openssl_encrypt($simple_string, $ciphering, 
-
-            $encryption_key, $options, $encryption_iv); 
-
-  
-// Display the encrypted string 
-
-echo "Encrypted String: " . $encryption . "\n"; 
-
-  
-// Non-NULL Initialization Vector for decryption 
-
-$decryption_iv = '1234567891011121'; 
-
-  
-// Store the decryption key 
-
-$decryption_key = "GeeksforGeeks"; 
-
-  
-// Use openssl_decrypt() function to decrypt the data 
-
-$decryption=openssl_decrypt ($encryption, $ciphering,  
-
-        $decryption_key, $options, $decryption_iv); 
-
-  
-// Display the decrypted string 
-
-echo "Decrypted String: " . $decryption; 
-
-  
+// Then we send the user off to authorize
+$url = $mwOAuthAuthorizeUrl;
+$url .= strpos( $url, '?' ) ? '&' : '?';
+$url .= http_build_query( array(
+'oauth_token' => $token->key,
+'oauth_consumer_key' => $gConsumerKey,
+) );
+header( "Location: $url" );
+echo 'Please see <a href="' . htmlspecialchars( $url ) . '">' . htmlspecialchars( $url ) . '</a>';
 ?> 
