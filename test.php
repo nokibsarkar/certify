351730720605ced24a0d66b76b17a9c9b18f69ac
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-10-30 10:46:38 +0600

    Add files via upload

--- test.php
+++ test.php
@@ -104,7 +104,7 @@ $signature = sign_request( 'GET', $url );
 $url .= "&oauth_signature=" . urlencode( $signature );
 $ch = curl_init();
 curl_setopt( $ch, CURLOPT_URL, $url );
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 curl_setopt( $ch, CURLOPT_USERAGENT, $gUserAgent );
 curl_setopt( $ch, CURLOPT_HEADER, 0 );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
