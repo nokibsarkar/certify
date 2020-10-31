dda71d6e4595228f89dfeefb0323cc5e19ad4125
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-10-31 17:23:56 +0600

    Add files via upload

---
+++ certify.php
@@ -0,0 +1,25 @@
+<?php
+$serial = isset($_REQUEST['i'])?(int)$_REQUEST['i']:0;
+if($serial){
+	$host = "tools.db.svc.eqiad.wmflabs";
+	$creds = parse_ini("../replica.my.cnf");
+	$conn = mysqli_connect($host,$creds['username'],$creds['password'],'s54548__certify');
+	if(!$conn)
+		die(mysqli_connect_error());
+	$sql = "SELECT *
+	FROM `Certificate`
+	JOIN Users
+	JOIN Workshop
+	WHERE Workshop.ID = Certificate.Event
+	AND Users.Username = Certificate.To
+	AND Certificate.ID = $serial";
+	$res = $conn->query($sql);
+	if($res){
+		$res = $res->fetch_assoc();
+		var_dump($res);
+	}
+}
+else{
+
+}
+?>
