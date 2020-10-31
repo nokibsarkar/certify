f47f8bb5e1c88911279c704928ece13a568fd9b5
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-10-31 19:37:16 +0600

    Add files via upload

--- certify.php
+++ certify.php
@@ -17,6 +17,50 @@ if($serial){
 	if($res){
 		$res = $res->fetch_assoc();
 		var_dump($res);
+		$ref = ["০", "১", " ২", "৩", " ৪", "৫"," ৬", "৭"," ৮", "৯"];
+		function en2bn($n = ''){
+			$n = (str)$n;
+			$l = strlen($l);
+			for($i=0;$i<$l;$i++)
+				$n[$i] = isset($ref[$n[$i]])?$ref[$n[$i]]:$n[$i];
+			return $n;
+		}
+		$res["Partner"] = json_decode($res["Partner"],true);
+		$data = ["bn"=>[
+			"name"=>$res["Bengali"],
+			"institution"=>$res["Institution"],
+			"email"=>$res["Email"]
+		],"en"=>[]];
+		$t = $data["Certificate"];
+		$l = strlen($t) ;
+		$i = 0;
+		$c = "";
+		$s = 0;
+		$r = "";
+		while($i < $l){
+			if($t[$i]=='$'){
+				if($s){
+					//end of the param
+					$r = explode(".",$r);
+					if(empty($r[1]))
+						$c .= '$'.$r[0].'$';
+					else
+						$c .= isset($data[$r[0]][$r[1]])?$data[$r[0]][$r[1]] : '$'.$r[0].".".$r[1].'$';
+					$r = "";
+				}
+			$i++;
+			$s = !$s;
+			}
+			if($i>=$l)
+				break;
+			if($s)
+				$r .= $t[$i];
+			else
+				$c .= $t[$i];
+			$i++;
+		}
+		unset($t);
+		echo $c;
 	}
 }
 else{
