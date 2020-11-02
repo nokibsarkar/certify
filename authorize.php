bd99361e91458265a1a69242ab4c86c8452f73fd
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-02 11:47:09 +0600

    Add files via upload

--- authorize.php
+++ authorize.php
@@ -202,7 +202,22 @@ $conn->query($sql);
 if(!$conn->affected_rows){
 	//Not yet registered so prompt for register
 register:
-echo "No update";
+?>
+<form action="authorize.php" method="post">
+	<label for="username">ব্যবহারকারী নাম : </label>
+	<input name="username" value="" readonly/><br/>
+	<label for="bn_name">আসল নাম (বাংলা)</label>
+	<input name="bn_name" required/><br/>
+	<label for="en_name">আসল নাম (ইংরেজি)</label>
+	<input name="en_name" required/><br/>
+	<label for="bn_inst">প্রতিষ্ঠান (বাংলা)</label>
+	<input name="bn_inst" required/><br/>
+	<label for="en_inst">প্রতিষ্ঠান (ইংরেজি)</label>
+	<input name="en_inst" required/><br/>
+	<input type="checkbox" name="policy"/> আপনি নিবন্ধনের মাধ্যমে উইকিমিডিয়া বাংলাদেশের গোপনীয়তা নীতির সঙ্গে সম্মত হচ্ছেন।
+	<input type="submit" value="নিবন্ধন"/>
+</form>
+<?php
 }
 }
 $return = isset($_SESSION['return'])?urldecode($_SESSION['return']):'index.php';
