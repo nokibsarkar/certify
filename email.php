c8d9fb391edbdcd41de6be9e1ddcfba748afa20b
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-03 16:01:09 +0600

    Add files via upload

--- email.php
+++ email.php
@@ -29,7 +29,8 @@ $creds = parse_ini_file("/data/project/certify/replica.my.cnf");
 $conn = mysqli_connect($host,$creds['user'],$creds['password'],'s54548__certify');
 $sql = "SELECT Users.Token_Key AS K, Users.Token_Secret AS S, Queue.Task AS T, Queue.ID AS I FROM Users JOIN Queue WHERE Users.Username = '".addslashes($argv[1])."' AND Users.Username = Queue.Initiator AND Queue.Type = 1 AND Queue.Status = 0";
 $res = $conn->query($sql);
-echo mysqli_error($conn);
+echo mysqli_error();
+echo $sql."\n";
 if(!($res = $res->fetch_assoc()))
 	exit("No task is Defined");
 $mail_list = json_decode($res["T"],true);
