2835deca27a2bd2d776f7369d0ba1d70b03ecdbf
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-03 16:37:13 +0600

    Add files via upload

--- email.php
+++ email.php
@@ -32,7 +32,7 @@ $res = $conn->query($sql);
 echo mysqli_error($conn);
 if(!($res = $res->fetch_assoc()))
 	exit("No task is Defined");
-echo $res["T"];
+echo json_decode($res["T"]);
 $mail_list = json_decode($res["T"],true);
 $gTokenKey = $res["K"];
 $gTokenSecret= $res["S"];
