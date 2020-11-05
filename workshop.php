0df41c3a3cc5d915324834da80d004e871748c81
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-05 19:36:45 +0600

    Update workshop.php

--- workshop.php
+++ workshop.php
@@ -183,6 +183,7 @@ function multiAdd(obj,name){
 		<input name="status" type="checkbox"/> <label for="status">সমাপ্ত</label>
 	</fieldset>
 	<input type="submit"/>
+        <a href="admin.php?ID=<?php echo res['ID'];?>">প্রতিযোগীদের সনদপত্র প্রদান</a>
 	</form>
 </body>
 </html>
