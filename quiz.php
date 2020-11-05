9aa79d3064429f433aaa4f2e7323fbc3d6b0187a
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-05 15:11:16 +0600

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -74,6 +74,8 @@ else{
 	if(!($res = $res->fetch_assoc()))
 		header("Location: workshop.php");
 	$now = time();
+	echo date_timestamp_get(date_create($res["Start"]));
+	echo "<br>$now";
 	$before = $now < date_timestamp_get(date_create($res["Start"]));
 	$question = ($question = json_decode($res["Quiz"],true))?$question:[];
 if($_SESSION["user"]["admin"]){
