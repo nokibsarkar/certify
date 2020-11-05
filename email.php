31f346a51ad9177863c8c5d0995c7d58203362c8
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-05 21:01:50 +0600

    Update email.php

--- email.php
+++ email.php
@@ -1,178 +1,90 @@
 <?php
-if(php_sapi_name()!="cli")
-	exit(http_response_code(403));
-// Read the ini file
-$inifile = '/data/project/certify/oauth.ini';
-$ini = parse_ini_file( $inifile );
-if ( $ini === false ) {
-	//header( "HTTP/1.1 $errorCode Internal Server Error" );
-	echo 'The ini file could not be read';
-	exit(0);
-}
-if(!isset($ini['gAgent']) ||
-!isset( $ini['gConsumerKey'] ) ||
- !isset( $ini['gConsumerSecret'] )
-){
-	//header( "HTTP/1.1 $errorCode Internal Server Error" );
-	echo 'Required configuration directives not found in ini file';
-	exit(0);
-}
-$gUserAgent = $ini['gAgent'];
-$gConsumerKey = $ini['gConsumerKey'];
-$gConsumerSecret = $ini['gConsumerSecret'];
-/*Fetch who initiated it */
-if($argc==1)
-	exit("No initiator given");
-/*Fetch from database*/
-$host = "tools.db.svc.eqiad.wmflabs";
-$creds = parse_ini_file("/data/project/certify/replica.my.cnf");
-$conn = mysqli_connect($host,$creds['user'],$creds['password'],'s54548__certify');
-$sql = "SELECT Users.Token_Key AS K, Users.Token_Secret AS S, Queue.Task AS T, Queue.ID AS I FROM Users JOIN Queue WHERE Users.Username = '".addslashes($argv[1])."' AND Users.Username = Queue.Initiator AND Queue.Type = 1 AND Queue.Status = b'00'";
-$res = $conn->query($sql);
-echo mysqli_error($conn);
-if(!($res = $res->fetch_assoc()))
-	exit("No task is Defined");
-eval('$mail_list = '.$res["T"].';');
-$gTokenKey = $res["K"];
-$gTokenSecret= $res["S"];
-$id = $res["I"];
-//Update the Status
-$sql = "UPDATE Queue SET Status = b'01' WHERE ID = $id";
-$conn->query($sql);
-$mwOAuthUrl = 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth';
-$mwOAuthIW = 'meta';
-
- function sign_request( $method, $url, $params = array() ) {
- global $gConsumerSecret, $gTokenSecret;
- $parts = parse_url( $url );
- // We need to normalize the endpoint URL
- $scheme = isset( $parts['scheme'] ) ? $parts['scheme'] : 'http';
- $host = isset( $parts['host'] ) ? $parts['host'] : '';
- $port = isset( $parts['port'] ) ? $parts['port'] : ( $scheme == 'https' ? '443' : '80' );
- $path = isset( $parts['path'] ) ? $parts['path'] : '';
- if ( ( $scheme == 'https' && $port != '443' ) ||
- ( $scheme == 'http' && $port != '80' ) 
- ) {
- // Only include the port if it's not the default
- $host = "$host:$port";
- }
- 
- // Also the parameters
- $pairs = array();
- parse_str( isset( $parts['query'] ) ? $parts['query'] : '', $query );
- $query += $params;
- unset( $query['oauth_signature'] );
- if ( $query ) {
- $query = array_combine(
- // rawurlencode follows RFC 3986 since PHP 5.3
- array_map( 'rawurlencode', array_keys( $query ) ),
- array_map( 'rawurlencode', array_values( $query ) )
- );
- ksort( $query, SORT_STRING );
- foreach ( $query as $k => $v ) {
- $pairs[] = "$k=$v";
- }
- }
- 
- $toSign = rawurlencode( strtoupper( $method ) ) . '&' .
- rawurlencode( "$scheme://$host$path" ) . '&' .
- rawurlencode( join( '&', $pairs ) );
- $key = rawurlencode( $gConsumerSecret ) . '&' . rawurlencode( $gTokenSecret );
- return base64_encode( hash_hmac( 'sha1', $toSign, $key, true ) );
- }
-$apiUrl = 'https://bn.wikipedia.org/w/api.php';
-/**
- * Send an API query with OAuth authorization
- *
- * @param array $post Post data
- * @param object $ch Curl handle
- * @return array API results
- */
-function doApiQuery( $post, &$ch = null ) {
-	global $apiUrl, $gUserAgent, $gConsumerKey, $gTokenKey, $errorCode;
-
-	$headerArr = array(
-		// OAuth information
-		'oauth_consumer_key' => $gConsumerKey,
-		'oauth_token' => $gTokenKey,
-		'oauth_version' => '1.0',
-		'oauth_nonce' => md5( microtime() . mt_rand() ),
-		'oauth_timestamp' => time(),
-
-		// We're using secret key signatures here.
-		'oauth_signature_method' => 'HMAC-SHA1',
-	);
-	$signature = sign_request( 'POST', $apiUrl, $post + $headerArr );
-	$headerArr['oauth_signature'] = $signature;
-
-	$header = array();
-	foreach ( $headerArr as $k => $v ) {
-		$header[] = rawurlencode( $k ) . '="' . rawurlencode( $v ) . '"';
+session_start();
+if(!isset($_SESSION["user"]["name"]))
+    header("location:login.php?return=".urlencode($_SERVER["REQUEST_URI"]));
+if(!$_SESSION["user"]["admin"])
+    exit(http_response_code(403));
+$user = $_SESSION["user"]["name"];
+$id = isset($_REQUEST["ID"])?(int)$_REQUEST["ID"]:0;
+if(!$id)
+	header("location: workshop.php");
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+	//form submitted
+	require 'parse.php';
+	$list = (int)$_POST["list"]; //the email list criteria
+	$data = $_POST["data"];
+	$subject = htmlspecialchars(addslashes($_POST["subject"]));
+	$body = $_POST["body"];
+	$sql = "SELECT Users.* FROM Users";
+	switch($list){
+		case 0:
+			$sql.=" JOIN Response WHERE Response.Workshop = $id AND Response.By = Users.Username AND Response.Score >= ".$data['minScore'];
+			break;
 	}
-	$header = 'Authorization: OAuth ' . join( ', ', $header );
-
-	if ( !$ch ) {
-		$ch = curl_init();
-	}
-	curl_setopt( $ch, CURLOPT_POST, true );
-	curl_setopt( $ch, CURLOPT_URL, $apiUrl );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $post ) );
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, array( $header ) );
-	//curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-	curl_setopt( $ch, CURLOPT_USERAGENT, $gUserAgent );
-	curl_setopt( $ch, CURLOPT_HEADER, 0 );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-	$data = curl_exec( $ch );
-	if ( !$data ) {
-		//header( "HTTP/1.1 $errorCode Internal Server Error" );
-		echo 'Curl error: ' . htmlspecialchars( curl_error( $ch ) );
-		//exit(0);
-	}
-	$ret = json_decode( $data, true);
-	if ( $ret == []) {
-	//	header( "HTTP/1.1 $errorCode Internal Server Error" );
-		echo 'Unparsable API response: <pre>' . htmlspecialchars( $data ) . '</pre>';
-		exit(0);
-	}
-	return $ret;
-}
-function fetch_token(){
-	$data = [
-	"action"=> "query",
-	"format"=> "json",
-	"meta"=> "tokens",
-	"type"=> "csrf"
+	$list = [];
+	$host = "tools.db.svc.eqiad.wmflabs";
+	$creds = parse_ini_file("../replica.my.cnf");
+	$conn = mysqli_connect($host,$creds['user'],$creds['password'],'s54548__certify');
+	$res = $conn->query($sql);
+	echo mysqli_error($conn);
+	echo $sql;
+	while($row = $res->fetch_assoc()){
+	$sql = "INSERT INTO Certificates VALUES (NULL , '".$row["Username"]."',CURRENT_TIMESTAMP , ".$id.");";
+	$conn->query($sql);
+	$serial = $conn->insert_id;
+	$t = [
+	"bn"=>[
+		"name" => $row["Bengali"],
+		"institution" => json_decode($row["Institution"],true)[0],
+		"serial" => en2bn($serial)
+	],
+		"en" => ["serial" => $serial]
 	];
-	$res = doApiQuery($data)["query"]["tokens"]["csrftoken"];
-	return $res=="+\\"?NULL : $res;
-	
-}
-$token = fetch_token();
-foreach($mail_list as $v){
-	send:
-	echo "Sending mail to : ".$v[0];
-	$data = [
-	"action"=> "emailuser",
-	"format"=> "json",
-	"target"=> $v[0],
-	"subject"=> $v[1],
-	"text"=> $v[2],
-	"token"=> $token
-	];
-	$res = doApiQuery($data);
-	if(isset($res["error"])){
-		if($res["error"]["code"]=="badtoken")
-			{
-				$token = fetch_token();
-				goto send;
-			}
-		echo $res["info"];
+	array_push($list,[
+		$row["Username"],
+		$subject,
+		parse($body,$t)
+	]);
 	}
-	else
-		echo "Sent to :".$v[0];
-	//Sleep for 5 seconds
-	sleep(5);
+	array_push($list,[
+	$user,
+	"আপনার দেয়া কাজ সম্পন্ন হয়েছে",
+	"আপনি আমায় ইমেল পাঠানোর জন্য যে কাজটি দিয়েছেন তা আমি সম্পন্ন করেছি। আমার উপর আস্থা রাখার জন্য অসংখ্য ধন্যবাদ।
+	-নকীব বট-
+	-নাজমুল হক নকীব-"
+	]);
+	$list = json_encode($list,JSON_UNESCAPED_UNICODE);
+	$sql = "INSERT INTO `Queue` VALUES (NULL, '$user', b'01', '$list', b'00');";
+	$conn->query($sql);
+	$id = $conn->insert_id;
+	$cm = "jsub -N T$id php -f /data/project/certify/public_html/email.php '$user'";
+	echo shell_exec($cm);
+	//echo en2bn($id)."নং কাজটি জমা দেয়া হয়েছে";
 }
-$sql = "UPDATE Queue SET Status = b'10' WHERE ID = $id";
-$conn->query($sql);
+else{
+?>
+<link rel="stylesheet" href="Styles/style.css">
+<link rel="stylesheet" href="Styles/form.css">
+<h1>ইমেইল ফর্ম</h1>
+<form action="admin.php" method="post">
+	<label>কর্মশালা নং</label><input type="text" placeholder="কর্মশালা নং" name="ID" value="<?php echo $id;?>" readonly/><br/>
+	<label for="subject">বিষয়:</label><input type="text" name="subject" placeholder="বিষয় লিখুন" value="আপনার অংশগ্রহণ সার্টিফিকেট"></input><br/>
+	<label for="body" class="unhide">বিষয়বস্তু</label>
+	<ul id="suggestions">
+	<input class="suggestion" type="button" onclick="in_body.value+=this.value" value="$bn.name$">
+	<input class="suggestion" type="button" onclick="in_body.value+=this.value" value="$en.name$">
+	</ul>
+	<textarea name id="in_body"> ওকে </textarea><br/>
+	<label for="list" class="unhide">ছাঁকনী:</label>
+	<select name="list">
+		<option value="0">উত্তীর্ণ অংশগ্রহণকারী</option>
+	</select>
+	<br>
+	<label for="data[minScore]" class="unhide">ন্যুনতম স্কোর:</label>
+	<input name="data[minScore]" type="range" min="0" max="100" step="0.5" onchange="value.innerHTML = this.value"/><font id="value"></font>
+	<br>
+	<input type="submit"/>
+</form>
+<?php
+}
 ?>
