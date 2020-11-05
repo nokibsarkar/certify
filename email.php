3e8eefa6227e07bb8b8e3fe299adf500c4f99bfd
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-05 21:09:38 +0600

    Update email.php

--- email.php
+++ email.php
@@ -30,7 +30,9 @@ $conn = mysqli_connect($host,$creds['user'],$creds['password'],'s54548__certify'
 $sql = "SELECT Users.Token_Key AS K, Users.Token_Secret AS S, Queue.Task AS T, Queue.ID AS I FROM Users JOIN Queue WHERE Users.Username = '".addslashes($argv[1])."' AND Users.Username = Queue.Initiator AND Queue.Type = 1 AND Queue.Status = 0";
 $res = $conn->query($sql);
 echo mysqli_error($conn);
-if(!($res = $res->fetch_assoc()))
+$res = $res->fetch_assoc()
+var_dump($res);
+if(!($res)
 	exit("No task is Defined");
 eval('$mail_list = '.$res["T"].';');
 $gTokenKey = $res["K"];
