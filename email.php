59d11d20d3ad4d22723a3ddc575d81debcb27ed9
Nokib Sarkar <39087050+nokibsarkar@users.noreply.github.com>
2020-11-06 15:23:56 +0600

    Update email.php

--- email.php
+++ email.php
@@ -27,7 +27,7 @@ if($argc==1)
 $host = "tools.db.svc.eqiad.wmflabs";
 $creds = parse_ini_file("/data/project/certify/replica.my.cnf");
 $conn = mysqli_connect($host,$creds['user'],$creds['password'],'s54548__certify');
-$sql = "SELECT Users.Token_Key AS K, Users.Token_Secret AS S, Users.Username AS U Queue.Task AS T, Queue.ID AS I FROM Users JOIN Queue WHERE Queue.ID = ".(int)$argv[1]." AND Users.Username = Queue.Initiator AND Queue.Type = 1 AND Queue.Status = 0";
+$sql = "SELECT Users.Token_Key AS K, Users.Token_Secret AS S, Queue.Task AS T, Queue.ID AS I FROM Users JOIN Queue WHERE Queue.ID = ".(int)$argv[1]." AND Users.Username = Queue.Initiator AND Queue.Type = 1 AND Queue.Status = 0";
 echo $sql;
 $res = $conn->query($sql);
 echo mysqli_error($conn);
